9bf194c506c96a28c655bf0a1c86c28635538945
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-01-23 16:34:41 +0530

    Update json.js

--- json.js
+++ json.js
@@ -1,14 +1,10 @@
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const xhr2 = new XMLHttpRequest();
-xhr2.onreadystatechange = () => {
-  if (xhr2.readyState === XMLHttpRequest.DONE) {
-    console.log(typeof xhr2.responseText);
-    console.log(xhr2.responseText);
-  }
-};
+
 xhr2.open('GET', 'https://www.boredapi.com/api/activity', true);
 xhr2.setRequestHeader('Accept', 'application/json');
+xhr.send(null);
 
 if (xhr2.readyState === XMLHttpRequest.DONE) {
   const res = JSON.parse(xhr2.responseText);
