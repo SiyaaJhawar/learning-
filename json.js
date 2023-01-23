960521e28e7e6f93292f3caae9e68a427abd3d2d
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-01-23 16:49:29 +0530

    Update json.js

--- json.js
+++ json.js
@@ -12,6 +12,6 @@ if (xhr2.readyState === XMLHttpRequest.DONE) {
 };
 const res = JSON.parse(xhr2.responseText);
 Object.entries(res).forEach((entry) => {
-  const [key, value] = entry;
+  const {key, value} = entry;
   console.log(`${key}: ${value}`);
 });
