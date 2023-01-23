af9d8aa6dc40b73dd9b3fc2ecf6f09af2f60047b
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-01-23 15:45:42 +0530

    Update json.js

--- json.js
+++ json.js
@@ -1,18 +1,20 @@
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = () => {
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    console.log(typeof xhr.responseText);
-    console.log(xhr.responseText);
+import XMLHttpRequest from 'xhr2';
+
+const xhr2 = new XMLHttpRequest();
+xhr2.onreadystatechange = () => {
+  if (xhr2.readyState === XMLHttpRequest.DONE) {
+    console.log(typeof xhr2.responseText);
+    console.log(xhr2.responseText);
   }
 };
-xhr.open('GET', 'https://icanhazdadjoke.com/', true);
-xhr.setRequestHeader('Accept', 'application/json');
-xhr.send(null);
-if (xhr.readyState === XMLHttpRequest.DONE) {
-  const res = JSON.parse(xhr.responseText);
+xhr2.open('GET', 'https://www.boredapi.com/api/activity', true);
+xhr2.setRequestHeader('Accept', 'application/json');
+xhr2.send(null);
+if (xhr2.readyState === XMLHttpRequest.DONE) {
+  const res = JSON.parse(xhr2.responseText);
   console.log(res);
 };
-const res = JSON.parse(xhr.responseText);
+const res = JSON.parse(xhr2.responseText);
 Object.entries(res).forEach((entry) => {
   const [key, value] = entry;
   console.log(`${key}: ${value}`);
