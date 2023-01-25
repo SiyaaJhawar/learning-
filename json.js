13f84a80b3889a2000336833b5345fc7621248f1
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-01-25 13:57:50 +0530

    Update json.js

--- json.js
+++ json.js
@@ -6,17 +6,27 @@ xhr.open("GET", "https://www.boredapi.com/api/activity",true);
 xhr.onreadystatechange = function () {
     console.log("readyState = " + this.readyState + ", status = " + this.status);
     if (this.readyState == 4 && this.status == 200) {
-        var result = this.responseText;
+        var result = JSON.parse(this.responseText);
         console.log(result);
-   var result = JSON.parse(this.responseText);
-     Object.keys(result).forEach(function(key) {
-    console.log(key + ": " + result[key]);
-});
-}
-    };
-
+        if(Array.isArray(result)){
+         for (var i = 0; i < result.length; i++) {
+            console.log(result[i]);
+            }
+        }else{
+            for(var key in result){
+                console.log(key + ': '+ result[key]);
+            }
+        }
+    }
+};
  
 xhr.send();
+
+
+
+
+
+
 
  
 
