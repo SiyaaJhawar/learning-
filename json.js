24c43f0c9089a0f9159ec73c83e5dcdf9108dcc7
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-01-27 14:44:02 +0530

    Update json.js

--- json.js
+++ json.js
@@ -10,12 +10,11 @@ xhr.onreadystatechange = function () {
         var result = JSON.parse(this.responseText);
         console.log(result);
       if(result.hasOwnProperty(obj) && Array.isArray(result.obj)){
-            result.obj.forEach(function(item) {
-                for(var key in item){
-                    console.log(key + ': '+ item[key]);
-                }
-            });
-        
+            result.forEach(obj => {
+        Object.entries(obj).forEach(([key, value]) => {
+            console.log(`${key} ${value}`);
+        });
+            }
     
         
     
