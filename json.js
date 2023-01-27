9fef65ce06dfae79cf18a04c81fca685933bef06
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-01-27 15:24:00 +0530

    Update json.js

--- json.js
+++ json.js
@@ -9,13 +9,14 @@ xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var result = JSON.parse(this.responseText);
         console.log(result);
-       if(result.hasOwnProperty('places') && Array.isArray(result.places)){
+        if(result.hasOwnProperty('places') && Array.isArray(result.places)){
             result.places.forEach(function(item) {
                 for(var key in item){
                     console.log(key + ': '+ item[key]);
                 }
             });
         }
+    }
     
     }else{
             for(var key in result){
