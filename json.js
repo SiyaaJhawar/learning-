faab2955ece796af50a69ae0f016ce340fc04352
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-03 11:25:41 +0530

    Update json.js

--- json.js
+++ json.js
@@ -1,42 +1,38 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
  
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.publicapis.org/entries",true);
+xhr.open("GET", "https://api.publicapis.org/entries", true);
 xhr.send();
-//xhr.open("GET", "https://www.boredapi.com/api/activity",false);
-//xhr.send();
  
 xhr.onreadystatechange = function () {
-    console.log("readyState = " + this.readyState + ", status = " + this.status);
-    if (this.readyState == 4 && this.status == 200) {
-        var result = JSON.parse(this.responseText);
-        console.log(result);
-    
+  console.log("readyState = " + this.readyState + ", status = " + this.status);
+  if (this.readyState === 4 && this.status === 200) {
+    var result = JSON.parse(this.responseText);
+    console.log(result);
+
     for (var key in result) {
-        if (result.hasOwnProperty(key)) {
-            var val = result[key];
-          console.log(result.hasOwnProperty(key));
-           if (Array.isArray(val)) {
-                val.forEach(function(item) {
-                    for (var k in item) {
-                       if (item.hasOwnProperty(k)) {
-                         console.log(item.hasOwnProperty(k));
-                           console.log(k + ": " + item[k]);
-                        }else {
-        console.log(item.hasOwnProperty(k));
-      }
-                     
-                   }
-               });
-           } else {
-                console.log(key + ": " + val);
+      if (result.hasOwnProperty(key)) {
+        var val = result[key];
+        console.log(result.hasOwnProperty(key));
+        if (Array.isArray(val)) {
+          val.forEach(function(item) {
+            for (var k in item) {
+              if (item.hasOwnProperty(k)) {
+                console.log(item.hasOwnProperty(k));
+                console.log(k + ": " + item[k]);
+              } else if (k === "somePropertyThatDoesNotExist") {
+                console.log(item.hasOwnProperty("somePropertyThatDoesNotExist"));
+                console.log(false);
+              }
             }
+          });
+        } else {
+          console.log(key + ": " + val);
         }
+      }
     }
-}
+  }
 };
-
-
 
 
 
