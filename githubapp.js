dd869b2c1066bd8779ffb0e220c79e05bccea5ab
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-06 15:49:16 +0530

    Update githubapp.js

--- githubapp.js
+++ githubapp.js
@@ -1,29 +1,30 @@
+const jwt = require('jsonwebtoken');
 const axios = require('axios');
 
-async function createGitHubIssue(SiyaaJhawar, learning, Hi, issueBody) {
-  try {
-    const response = await axios.post(
-      `https://api.github.com/repos/SiyaaJhawar/learning-/issues`,
-      {
-        title: Hi,
-        body: issueBody
-      },
-      {
-        headers: {
-          Accept: 'application/vnd.github+json',
-          Authorization: `Token ${process.env.GITHUB_ACCESS_TOKEN}`
-        }
-      }
-    );
+const privateKey = 'SHA256:tzGK/2qqqpwA+RL5vNtRlt+i1WMU/aEWxGJjdWBK/Ow=';
+const payload = {
+  iat: Math.floor(Date.now() / 1000),
+  exp: Math.floor(Date.now() / 1000) + (10 * 60),
+  iss: '281301',
+};
+const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+
+const headers = {
+  Authorization: `Bearer ${token}`,
+  'User-Agent': 'SiyaaJhawar',
+};
+
+axios
+  .post('https://api.github.com/repos/SiyaaJhawar/learning-/issues', {
+    title: 'Hi',
+    body: 'This is a sample code',
+  }, { headers })
+  .then((response) => {
     console.log(response.data);
-  } catch (error) {
+  })
+  .catch((error) => {
     console.error(error);
-  }
-}
-
-createGitHubIssue('SiyaaJhawar', 'learning', 'Hi', 'Issue body');
-
-
+  });
 
     
 
