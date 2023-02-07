9bceaa08cd724f6fb65ecb4df32fd3f9c39c4839
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-07 10:56:50 +0530

    Update githubapp.js

--- githubapp.js
+++ githubapp.js
@@ -15,7 +15,7 @@ const headers = {
 };
 
 axios
-  .post('https://api.github.com/repos/SiyaaJhawar/learning-/issues', {
+  .post('http://api.github.com/repos/SiyaaJhawar/learning-/issues', {
     title: 'Hi',
     body: 'This is a sample code',
   }, { headers })
