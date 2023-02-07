0b2c4e7b129a91b056da2866c13e429f463d8ee4
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-07 14:40:20 +0530

    Create github.js

---
+++ github.js
@@ -0,0 +1,21 @@
+const Octokit = require("@octokit/rest");
+
+const octokit = new Octokit({
+  auth: "SHA256:tNMsW3nOASQbbaPQb/gJq4KUlwdrP4GPe+hpakZxx1w=",
+});
+
+async function createIssue(SiyaaJhawar, learning, Hi, body) {
+  try {
+    const issue = await octokit.issues.create({
+      SiyaaJhawar,
+      learning-,
+      Hi,
+      body,
+    });
+    console.log(`Issue created with ID: ${issue.data.id}`);
+  } catch (error) {
+    console.error(`Error creating issue: ${error}`);
+  }
+}
+
+createIssue("SiyaaJhawar", "learning-", "Hi", "Issue body");
