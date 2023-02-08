40142c5a7255b07cb530ed44fd563278d19fd166
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-08 12:04:08 +0530

    Create Installation.js

---
+++ Installation.js
@@ -0,0 +1,25 @@
+const Octokit = require("@octokit/rest");
+
+const octokit = new Octokit({
+  auth: "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"
+});
+
+async function getInstallationId(repo) {
+
+  const installations = await octokit.apps.listInstallations({});
+
+
+  const installation = installations.data.find(installation => {
+    return installation.account.login === repo.owner.login;
+  });
+
+ 
+  const token = await octokit.apps.createInstallationToken({
+    installation_id: installation.id
+  });
+
+  console.log(`Installation ID: ${installation.id}`);
+  console.log(`Installation token: ${token.data.token}`);
+}
+
+getInstallationId({ owner: { login: "SiyaaJhawar" } });
