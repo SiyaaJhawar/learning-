8cd9a69efca6753d6b86d3308f3646a0f7bb0ffe
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-08 16:30:35 +0530

    Update Installation.js

--- Installation.js
+++ Installation.js
@@ -1,27 +1,18 @@
+const axios = require("axios");
 
-const axios = require('axios');
-const fs = require("fs");
+const appId = "281301";
 
-const appId = '281301';
 
-const privateKey = fs.readFileSync("private_key.pem","utf8");
 
-const installationId = '1';
-
-async function getInstallationToken() {
-  const jwt = require('jsonwebtoken');
-  const now = Math.floor(Date.now() / 1000);
-  const payload = {
-    iat: now,
-    exp: now + (10 * 60),
-    iss: appId
-  };
-  const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
-
-  axios.get(`https://api.github.com/app/installations/${installationId}/access_tokens`, { headers })
+axios.get(`https://api.github.com/app/installations`, {})
   .then(response => {
-    console.log(response.data.token);
+    console.log(response.data);
   })
   .catch(error => {
     console.error(error);
   });
+
+
+
+
+
