fc3ad8ed1a11739b6797127061ba2f2f32133313
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-08 16:50:16 +0530

    Update Installation.js

--- Installation.js
+++ Installation.js
@@ -2,9 +2,13 @@ const axios = require("axios");
 
 const appId = "281301";
 
+const headers = {
+ const headers = {
+  Accept: "application/vnd.github+json",
+  Authorization: `Bearer <jwt_token>`
+};
 
-
-axios.get(`https://api.github.com/app/installations`, {})
+axios.get(`https://api.github.com/app/installations`, {headers})
   .then(response => {
     console.log(response.data);
   })
