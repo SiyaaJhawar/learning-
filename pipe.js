c1fb6c1155f80e69ab62705c96645ac5de423215
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-02-10 15:19:42 +0530

    Create pipe.js

---
+++ pipe.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+
+// Replace with your GitHub App ID
+const appId = $secrets.APP_ID;
+
+// Replace with the private key for your GitHub App
+const privateKey = $secrets.PRIVATE_KEY;
+
+
+const repo = "SiyaaJhawar/learning-";
+
+
+const sha = "be836ec";
+
+
+const url = `https://api.github.com/repos/SiyaaJhawar/learning-/commits//statuses`;
+
+
+const token = jwt.sign({ iat: Math.floor(Date.now() / 1000), exp: Math.floor(Date.now() / 1000) + (10 * 60), iss: appId }, privateKey, { algorithm: 'RS256' });
+
+// Set the headers
+const headers = {
+  "User-Agent": "SiyaaJhawar",
+  "Authorization": `Bearer ${token}`,
+  "Accept": "application/vnd.github+json"
+};
+
+
+axios.get(url, { headers })
+  .then(response => {
+  
+    const pipelineStatuses = response.data.filter(status => status.context === "ci/pipeline");
+
+   
+    console.log(pipelineStatuses);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+
+
+
+Regenerate response
