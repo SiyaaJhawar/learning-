2b9f2ccda592202314082cecb983f530dba9c3e3
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-11 18:26:25 +0530

    Update addlabel.js

--- addlabel.js
+++ addlabel.js
@@ -24,7 +24,7 @@ async function compareCommitCommentWithJiraIssue() {
 
     async function addLabelToIssue(defectId) {
       try {
-        const issueResponse = await axios.get(`${jiraUrl}/search?filter=allissues=text~"${defectId}"`, {
+        const issueResponse = await axios.get(`${jiraUrl}/search?filter=allissues', {
           headers: {
             "Authorization": `Basic ${btoa(`${username}:${password}`)}`,
             "Accept": "application/json"
