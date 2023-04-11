080fbd22b09f5f44091827bed6a258d3b7849dfb
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-11 15:35:43 +0530

    Update addlabel.js

--- addlabel.js
+++ addlabel.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import btoa from 'btoa';
+const axios = require('axios');
+const btoa = require('btoa');
 
 const jiraUrl = 'https://swgup.atlassian.net/rest/api/2/search/?filter=allissues';
 const githubUrl = 'https://api.github.com/repos/SiyaaJhawar/action/commits/7ba17fe7086423a30485d2949cf32255bc2c479d/comments';
