163814b682f434a39f0ddb33c5cb3eeed6f31161
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-12 12:43:38 +0530

    Update addlabel.js

--- addlabel.js
+++ addlabel.js
@@ -6,7 +6,7 @@ const btoa = require('btoa');
  
 //var xhr = new XMLHttpRequest();
 
-const jiraUrl = 'https://swgup.atlassian.net/rest/api/2';
+const jiraUrl = 'https://swgup.atlassian.net/rest/api/2/search?filter=allissues';
 const githubUrl = 'https://api.github.com/repos/SiyaaJhawar/action/commits/7ba17fe7086423a30485d2949cf32255bc2c479d/comments';
 const jiraUsername = process.env.JIRA_USERNAME;
 const jiraPassword = process.env.JIRA_API_TOKEN;
@@ -14,21 +14,6 @@ const username = process.env.GITHUB_USERNAME;
 const password = process.env.GITHUB_API_TOKEN;
 
 const defectRegex = /([A-Z]{1}[A-Z]{2,})-\d+/g;
-
-async function getAllIssuesForProject(projectKey) {
-  try {
-   
-    const response = await axios.get(`${jiraUrl}/search?jproject=SWT`, {
-      headers: {
-        "Authorization": `Basic ${btoa(`${jiraUsername}:${jiraPassword}`)}`,
-        "Accept": "application/json"
-      }
-    });
-    return response.data.issues;
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 async function compareCommitCommentWithJiraIssue() {
   try {
@@ -42,22 +27,41 @@ async function compareCommitCommentWithJiraIssue() {
     const defectIds = commentTexts.flatMap(text => text.match(defectRegex));
     console.log(`Found the following defect IDs in commit comments: ${defectIds}`);
 
-    const projectKey = 'SWT'; // example project key
-    const issues = await getAllIssuesForProject(projectKey);
-    const matchingIssues = issues.filter(issue => defectIds.includes(issue.key));
-    console.log(`Found the following issues in Jira for the defect IDs: ${matchingIssues.map(issue => issue.key)}`);
-    return matchingIssues;
+    const jiraIssuesResponse = await axios.get(jiraUrl, {
+      headers: {
+        "Authorization": `Basic ${btoa(`${jiraUsername}:${jiraPassword}`)}`,
+        "Accept": "application/json"
+      },
+      params: {
+        jql: `key in (${defectIds.join(',')})`
+      }
+    });
+    const jiraIssues = jiraIssuesResponse.data.issues;
+
+    for (const issue of jiraIssues) {
+      if (defectIds.includes(issue.key)) {
+        await axios.post(`${jiraUrl}/issue/${issue.key}/label`, {
+          name: "int_deploy"
+        }, {
+          headers: {
+            "Authorization": `Basic ${btoa(`${jiraUsername}:${jiraPassword}`)}`,
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+          }
+        });
+        console.log(`Added label 'int_deploy' to Jira issue ${issue.key}`);
+      }
+    }
   } catch (error) {
     console.error(error);
   }
 }
 
-async function getMatchingIssues() {
-  const matchingIssues = await compareCommitCommentWithJiraIssue();
-  console.log(matchingIssues);
-}
+compareCommitCommentWithJiraIssue();
 
-getMatchingIssues(); // call the function to retrieve and log the matching issues
+
+
+
 
 
 
