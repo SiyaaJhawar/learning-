5df660bdd24aadc8d9d27e71e97b450edc331e5d
SiyaaJhawar <119915532+SiyaaJhawar@users.noreply.github.com>
2023-04-12 15:52:34 +0530

    Update addlabel.js

--- addlabel.js
+++ addlabel.js
@@ -22,57 +22,51 @@ const defectRegex = /([A-Z]{1}[A-Z]{2,})-\d+/g;
 async function compareCommitCommentWithJiraIssue() {
   try {
    const encodedCredentials = Buffer.from(`${username}:${password}`).toString('base64');
-const commitsResponse = await axios.get(githubUrl, {
-  headers: {
-    "Authorization": `Basic ${encodedCredentials}`,
-    "Accept": "application/json"
-  }
-});
+   const commitsResponse = await axios.get(githubUrl, {
+      headers: {
+        "Authorization": `Basic ${encodedCredentials}`,
+        "Accept": "application/json"
+      }
+   });
 
     const commentTexts = commitsResponse.data.map(comment => comment.body);
     const defectIds = commentTexts.flatMap(text => text.match(defectRegex));
     console.log(`Found the following defect IDs in commit comments: ${defectIds}`);
-   console.log(`Username: ${jiraUsername}`);
-console.log(`Apitoken: ${jiraapitoken}`);
-fetch('https://swgup.atlassian.net/rest/api/3/search?filter=allissues', {
-  method: 'GET',
-  headers: {
-    'Authorization': `Basic ${Buffer.from(
-      'jiraUsername:<jiraapitoken>'
-    ).toString('base64')}`,
-    'Accept': 'application/json'
-  }
-})
-.then(response => {
-  console.log(
-    `Response: ${response.status} ${response.statusText}`
-  );
-  return response.json(); // Parse the response as JSON
-})
+    console.log(`Username: ${jiraUsername}`);
+    console.log(`Apitoken: ${jiraapitoken}`);
 
- .then(data => {
-  if (data.issues && data.issues.length > 0) {
-    const issueKeys = data.issues.map(issue => issue.key); // Extract the keys of all the issues
-    console.log(`Found the following issue keys: ${issueKeys.join(', ')}`);
-  } else {
-    console.log('No issues found in response.');
-  }
-})
-.catch(error => {
-  console.error('Error fetching issues:', error);
-});
-
-.catch(err => console.error(err));
-
+    fetch('https://swgup.atlassian.net/rest/api/3/search?filter=allissues', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Basic ${Buffer.from(
+          `${jiraUsername}:${jiraapitoken}`
+        ).toString('base64')}`,
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log(
+        `Response: ${response.status} ${response.statusText}`
+      );
+      return response.json(); // Parse the response as JSON
+    })
+    .then(data => {
+      if (data.issues && data.issues.length > 0) {
+        const issueKeys = data.issues.map(issue => issue.key); // Extract the keys of all the issues
+        console.log(`Found the following issue keys: ${issueKeys.join(', ')}`);
+      } else {
+        console.log('No issues found in response.');
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching issues:', error);
+    });
   } catch (error) {
     console.error(error);
   }
 }
 
 compareCommitCommentWithJiraIssue();
-
-
-
 
 
 
